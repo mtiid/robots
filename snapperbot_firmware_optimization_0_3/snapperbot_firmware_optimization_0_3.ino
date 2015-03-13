2f766d857505e0037de0c744913cd59150e8fda0
Nathan Shaw <nathanshawsemail@gmail.com>
2015-03-13 02:24:32 -0700

    worked more on optimization

--- snapperbot_firmware_optimization_0_3/snapperbot_firmware_optimization_0_3.ino
+++ snapperbot_firmware_optimization_0_3/snapperbot_firmware_optimization_0_3.ino
@@ -4,18 +4,16 @@
 // Spring 2015
 // CalArts MTIID
 //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
+//
 #define unit8 FLIP 0
-#define unit LOUD 1
+#define unit8 LOUD 1
 #define unit8 VERY 2
-
-//arrays for each snapper array
+//
 static int snapper1[8] = {22, 23, 24, 25, 26, 27, 28, 29};
 static int snapper2 [8] = {30, 31, 32, 33, 34, 35, 36, 37};
 static int snapper3 [8] = {38, 39, 40, 41, 42, 43, 44, 45};
 static int snapper4 [8] = {46, 47, 48, 49, 50, 51, 52, 53};
-
-uint8 snappers[4];//use the registers for the chip
+//
 uint8 snapperStates[4];//the states of the snappers
 uint8 incommingByte;
 //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -25,15 +23,31 @@ uint8 incommingByte;
 void setup() {
   Serial.begin(57600);
   for (int i = 0; i < 4; i++) {
-    snappers[i] = 0xFF;
+    snapperStates[i] = 0xFF;
+  }
+  PORTA = snapperStates[
+}
+        //
+        //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        //                        Intrument Commands
+        //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        //
+void setPorts(int which) {
+  if (which == 0) {
+    PORTA = snapperStates[0];
+  }
+  else if (which == 1) {
+    PORTB = snapperStates[1];
+  }
+  else if (which == 2) {
+    PORTC = snapperStates[2];
+  }
+  else if (which == 3) {
+    PORTD = snapperStates[3];
   }
 }
-//
-//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//                        Intrument Commands
-//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//
-void flipSwitch(unit8 array, unit8 swit) {
+
+void flipSwitch(byte array, byte swit) {
   if (array == 0) {
     PORTA = (snapperStates[array] ^ ((1 << (swit - 1)));
   }
@@ -46,69 +60,68 @@ void flipSwitch(unit8 array, unit8 swit) {
   else if (array == 3) {
     PORTD = (snapperStates[array] ^ ((1 << (swit - 1)));
   }
-}
-//
-void veryLoud(unit8 level) {
-  for (int i = 0; i < 4; i++) {
-    snapperStates[i] ^= (255 >> (8 - level));
-  }
-  PORTA = snapperStates[0];
-  PORTB = snapperStates[1];
-  PORTC = snapperStates[2];
-  PORTD = snapperStates[3];
-}
-//
-void loud(unit8 snapArray, unit8 level) {
-  snapperStates[snapArray] ^= (255 >> (8 - level));
-  //turn level into a bitmask
-  if (snapArray == 0) {
+  else if (array == 4) {
     PORTA = snapperStates[0];
-  }
-  else if (snapArray == 1) {
     PORTB = snapperStates[1];
-  }
-  else if (snapArray == 2) {
     PORTC = snapperStates[2];
-  }
-  else if (snapArray == 3) {
     PORTD = snapperStates[3];
   }
-}
-//
-//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//                              Serial Stuff
-//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//
-void byteListener() {
-  if (Serial.available()) {
-    while (Serial.available() {
-    incommingByte = Serial.read();
-      parseSerial(incommingByte);
+  //
+  void veryLoud(byte level) {
+    for (int i = 0; i < 4; i++) {
+      snapperStates[i] ^= (255 >> (8 - level));
     }
   }
-}
-//
-void parseSerial(uint8 data) {
-  //mode is the two most significan bi
-  unit8 mode = (data >> 6);
   //
-  if (mode == FLIP) {
-    flipSwitch((data & 0x30) >> 4, (1 >> (data & 0x0C));
+  void loud(uint8 snapArray, uint8 level) {
+    snapperStates[snapArray] ^= (255 >> (8 - level));
+    //turn level into a bitmask
+    if (snapArray == 0) {
+      PORTA = snapperStates[0];
+    }
+    else if (snapArray == 1) {
+      PORTB = snapperStates[1];
+    }
+    else if (snapArray == 2) {
+      PORTC = snapperStates[2];
+    }
+    else if (snapArray == 3) {
+      PORTD = snapperStates[3];
+    }
   }
-  else if (mode == LOUD) {
-    loud((data & 0x30) >> 4, (data & 0x0E) >> 1);
+  //
+  //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  //                              Serial Stuff
+  //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  //
+  void byteListener() {
+    if (Serial.available()) {
+      while (Serial.available() {
+      incommingByte = Serial.read();
+        parseSerial(incommingByte);
+      }
+    }
   }
-  else if (mode == VERY) {
-    veryLoud(data & 0x70 >> 3);
+  //
+  void parseSerial(uint8 data) {
+    //mode is the two most significan bi
+    unit8 mode = (data >> 6);
+    //
+    if (mode == FLIP) {
+      flipSwitch((data & 0x30) >> 4, (1 >> (data & 0x0C));
+    }
+    else if (mode == LOUD) {
+      loud((data & 0x30) >> 4, (data & 0x0E) >> 1);
+    }
+    else if (mode == VERY) {
+      veryLoud(data & 0x70 >> 3);
+    }
   }
-}
-//
-//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//                               Main Loop
-//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//
-void loop() {
-  byteListener();
-}
-
-
+  //
+  //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  //                               Main Loop
+  //////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  //
+  void loop() {
+    byteListener();
+  }
